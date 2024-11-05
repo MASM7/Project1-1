39e34ce3e121fb287a441470a15c57acdaeebe75
Mohammed <mohammed22be@gmail.com>
2024-11-05 18:10:13 +0300

    second commit

--- main.c
+++ main.c
@@ -1,64 +1,55 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
 void assembler(const char *inputFile, const char *outputFile);
 
 int main() {
-  
-    assembler("Fibonacci.asm", "binary.bin");
+    assembler("fibonacci.asm", "fibonacci.bin");
     return 0;
 }
 
 void assembler(const char *inputFile, const char *outputFile) {
-    FILE *fp2;
-    FILE *fp;
+    FILE *fp = fopen(inputFile, "r");
+    FILE *fp2 = fopen(outputFile, "w");
     char line[50];
 
-    
-    fp = fopen(inputFile, "r");
-    fp2 = fopen(outputFile, "w");
-
-    
     if (fp == NULL) {
         printf("File %s not found!\n", inputFile);
         return;
     }
-
-    
     if (fp2 == NULL) {
         printf("Could not open output file %s!\n", outputFile);
         fclose(fp);
         return;
     }
 
-    
     while (fgets(line, sizeof(line), fp) != NULL) {
-        line[strcspn(line, "\n")] = 0;
+        line[strcspn(line, "\n")] = 0;  // Remove newline character
 
-        //spaces
+        // Remove spaces from the line for uniform comparison
         char command[50];
-        strcpy(command, line);
-        for (char *p = command; *p; ++p) {
-            if (*p == ' ') {
-                memmove(p, p + 1, strlen(p));
-                p--; 
+        int j = 0;
+        for (int i = 0; line[i] != '\0'; i++) {
+            if (line[i] != ' ') {
+                command[j++] = line[i];
             }
         }
+        command[j] = '\0';  // Null-terminate the command string
 
-        
+        // Handle standard instructions
         if (strcmp(command, "RA=RA+RB") == 0) {
             fprintf(fp2, "00000000\n");
         } else if (strcmp(command, "RB=RA+RB") == 0) {
             fprintf(fp2, "00010000\n");
-        } else if (strcmp(command, "RA=RA-RB") == 0) { 
+        } else if (strcmp(command, "RA=RA-RB") == 0) {
             fprintf(fp2, "00000100\n");
         } else if (strcmp(command, "RB=RA-RB") == 0) {
             fprintf(fp2, "00010100\n");
         } else if (strcmp(command, "RO=RA") == 0) {
             fprintf(fp2, "00100000\n");
 
-        
+        // Handle immediate values for RA, RB, JC, J using switch cases for each command type
         } else if (strncmp(command, "RA=", 3) == 0) {
             int imm = atoi(command + 3);
             switch (imm) {
@@ -70,10 +61,8 @@ void assembler(const char *inputFile, const char *outputFile) {
                 case 5: fprintf(fp2, "00001101\n"); break;
                 case 6: fprintf(fp2, "00001110\n"); break;
                 case 7: fprintf(fp2, "00001111\n"); break;
-                default: printf("Uncategorized immediate value: %d\n", imm); break;
+                default: printf("Uncategorized immediate value for RA: %d\n", imm); break;
             }
-
-        
         } else if (strncmp(command, "RB=", 3) == 0) {
             int imm = atoi(command + 3);
             switch (imm) {
@@ -85,12 +74,10 @@ void assembler(const char *inputFile, const char *outputFile) {
                 case 5: fprintf(fp2, "00011101\n"); break;
                 case 6: fprintf(fp2, "00011110\n"); break;
                 case 7: fprintf(fp2, "00011111\n"); break;
-                default: printf("Uncategorized immediate value: %d\n", imm); break;
+                default: printf("Uncategorized immediate value for RB: %d\n", imm); break;
             }
-
-        
         } else if (strncmp(command, "JC=", 3) == 0) {
-            int imm = atoi(command + 3); 
+            int imm = atoi(command + 3);
             switch (imm) {
                 case 0: fprintf(fp2, "01110000\n"); break;
                 case 1: fprintf(fp2, "01110001\n"); break;
@@ -100,12 +87,10 @@ void assembler(const char *inputFile, const char *outputFile) {
                 case 5: fprintf(fp2, "01110101\n"); break;
                 case 6: fprintf(fp2, "01110110\n"); break;
                 case 7: fprintf(fp2, "01110111\n"); break;
-                default: printf("Uncategorized immediate value: %d\n", imm); break;
+                default: printf("Uncategorized immediate value for JC: %d\n", imm); break;
             }
-
-        
         } else if (strncmp(command, "J=", 2) == 0) {
-            int imm = atoi(command + 2); 
+            int imm = atoi(command + 2);
             switch (imm) {
                 case 0: fprintf(fp2, "10110000\n"); break;
                 case 1: fprintf(fp2, "10110001\n"); break;
@@ -115,12 +100,11 @@ void assembler(const char *inputFile, const char *outputFile) {
                 case 5: fprintf(fp2, "10110101\n"); break;
                 case 6: fprintf(fp2, "10110110\n"); break;
                 case 7: fprintf(fp2, "10110111\n"); break;
-                default: printf("Uncategorized immediate value: %d\n", imm); break;
+                default: printf("Uncategorized immediate value for J: %d\n", imm); break;
             }
         }
     }
 
-    
     fclose(fp);
     fclose(fp2);
 }
